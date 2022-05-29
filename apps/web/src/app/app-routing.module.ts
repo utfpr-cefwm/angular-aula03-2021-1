649b3e04c7154c55e7d35dc900b3b43af8630d9b
Rui Leite <ruipimentelleite@gmail.com>
2022-05-29 17:41:20 +0000

    Configura a sub-rota de listagem de artigos.

--- apps/web/src/app/app-routing.module.ts
+++ apps/web/src/app/app-routing.module.ts
@@ -2,16 +2,28 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 
 import { HomeComponent } from "./components/home/home.component";
+import { ListaArtigosComponent } from "./components/lista-artigos/lista-artigos.component";
 
 const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
-    redirectTo: '/home',
+    redirectTo: '/home/lista-artigos',
   },
   {
     path: 'home',
     component: HomeComponent,
+    children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'lista-artigos',
+      },
+      {
+        path: 'lista-artigos',
+        component: ListaArtigosComponent,
+      },
+    ],
   },
   {
     path: '**',
