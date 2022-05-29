5454a3af7db64efc6dda1fbfac01e081e80a0101
Rui Leite <ruipimentelleite@gmail.com>
2022-05-29 14:54:41 +0000

    Cria o `HomeComponent` e configura sua rota.
    - Comando:
      npx nx generate @schematics/angular:component components/home --project=web --module=app --no-interactive
    - Configuradas as rotas no `app-routing.module.ts`.

--- apps/web/src/app/app.module.ts
+++ apps/web/src/app/app.module.ts
@@ -3,9 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
