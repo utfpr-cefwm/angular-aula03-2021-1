4a4fb5d129bc223fc1cbdd19339cd265ac6f30a9
Rui Leite <ruipimentelleite@gmail.com>
2022-05-29 20:27:38 +0000

    Gera `ArtigoEdicaoModule`.
    - Comando:
      npx nx generate @schematics/angular:module modules/artigo-edicao --project=web --module=app --routing --no-interactive

---
+++ apps/web/src/app/modules/artigo-edicao/artigo-edicao.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ArtigoEdicaoRoutingModule } from './artigo-edicao-routing.module';
+
+@NgModule({
+  declarations: [
+  ],
+  imports: [
+    CommonModule,
+    ArtigoEdicaoRoutingModule,
+  ],
+})
+export class ArtigoEdicaoModule {
+}
