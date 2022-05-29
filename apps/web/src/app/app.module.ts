f5c1c59a691a3c32f1cbbc2bfd2efef8f62dcc48
Rui Leite <ruipimentelleite@gmail.com>
2022-05-29 17:35:39 +0000

    Cria o `ListaArtigosComponent`.
    - Comando:
      npx nx generate @schematics/angular:component components/lista-artigos --project=web --module=app --no-interactive

--- apps/web/src/app/app.module.ts
+++ apps/web/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { TabMenuModule } from 'primeng/tabmenu';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { ListaArtigosComponent } from './components/lista-artigos/lista-artigos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    ListaArtigosComponent,
   ],
   imports: [
     BrowserModule,
