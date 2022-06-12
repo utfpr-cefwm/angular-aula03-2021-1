efef8852df3595958e5af0bfcf8e32a171c8faa9
Rui Leite <ruipimentelleite@gmail.com>
2022-06-12 20:54:21 +0000

    Cria formulário de login.

--- apps/web/src/app/modules/auth/auth.module.ts
+++ apps/web/src/app/modules/auth/auth.module.ts
@@ -1,5 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './components/login/login.component';
@@ -10,7 +14,10 @@ import { LoginComponent } from './components/login/login.component';
   ],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     AuthRoutingModule,
+    InputTextModule,
+    ButtonModule,
   ],
 })
 export class AuthModule {
--- apps/web/src/app/modules/auth/components/login/login.component.ts
+++ apps/web/src/app/modules/auth/components/login/login.component.ts
@@ -1,4 +1,14 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { MessageService } from 'primeng/api';
+
+import { of } from 'rxjs';
+import { catchError, take } from 'rxjs/operators';
+
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'cefwm-angular-login',
@@ -7,9 +17,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public formGroup: FormGroup = this.formBuilder.group({
+    username: ['', Validators.required],
+    password: ['', Validators.required],
+  });
+
+  constructor(
+    private authService: AuthService,
+    private formBuilder: FormBuilder,
+    private messageService: MessageService,
+    private router: Router,
+  ) {
+  }
 
   ngOnInit(): void {
+  }
+
+  public login(): void {
+    this.authService.auth(this.formGroup.value).pipe(
+      take(1),
+      catchError((err: HttpErrorResponse) => {
+        return of(undefined);
+      }),
+    ).subscribe((resultado: ({token: string} | undefined)) => {
+      if (!resultado) {
+        this.messageService.add({
+          severity: 'error',
+          summary: 'Login ou senha incorreto',
+          detail: 'Tente novamente',
+        });
+      } else {
+        this.messageService.add({
+          severity: 'success',
+          summary: 'Login efetuado com sucesso!',
+          detail: 'Seja-bem vindo!',
+        });
+        this.router.navigate(['/']);
+        localStorage.setItem('token', resultado.token);
+      }
+    });
   }
 
 }
